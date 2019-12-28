f48c1a99574aa12287d9475da6001c27b5af8475
sanket8358 <sanketpatwardhan8358@gmail.com>
2019-12-29 00:00:05 +0530

    init commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+## This project is an example of push notifications using Spring Boot, Rest and JPA.
+
+In this project i use OneSignal API [https://onesignal.com/].
+You need to create an account on OneSignal to has a Rest API key
+to manage your notifications.
+
+To run this project: 
+        1 - clone the repository
+        2 - run on terminal mvn clean install
+        3 - run mvn spring-boot:run
---
+++ src/test/java/com/pushnotification/PushNotificationApplicationTests.java
@@ -0,0 +1,19 @@
+package com.pushnotification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Weslei Dias
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PushNotificationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
